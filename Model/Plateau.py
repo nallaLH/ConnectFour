adfa6d5f6821404225b7f6d4c8e105f518384904
lahe0010 <allan.laheu@etudiant.univ-reims.fr>
2024-01-14 23:08:17 +0100

    Ajout de la fonction encoderPlateau

--- Model/Plateau.py
+++ Model/Plateau.py
@@ -71,9 +71,9 @@ def placerPionPlateau(plateau: list, pion: dict, column: int) -> int:
     ligne_i = const.NB_LINES - 1
     res = -1
 
-    while (plateau[ligne_i][column] != None) and (ligne_i > 0):
+    while (plateau[ligne_i][column] is not None) and (ligne_i > 0):
         ligne_i -= 1
-    if plateau[ligne_i][column] == None:
+    if plateau[ligne_i][column] is None:
         plateau[ligne_i][column] = pion
         res = ligne_i
     return res
@@ -188,28 +188,6 @@ def detecter4diagonaleDirectePlateau(plateau: list, couleur: int) -> list:
     if couleur not in const.COULEURS:
         raise ValueError(f"detecter4diagonaleDirectePlateau : Le second paramètre {couleur} n’est pas une couleur")
 
-    # res = []
-    # i = 0
-    # while i <= (const.NB_LINES - 4):
-    #     j = 0
-    #     while j <= (const.NB_COLUMNS - 4):
-    #
-    #         if (plateau[i][j] is not None and getCouleurPion(plateau[i][j]) == couleur) and (plateau[i+1][j+1] is not None and getCouleurPion(plateau[i+1][j+1]) == couleur) and (plateau[i+2][j+2] is not None and getCouleurPion(plateau[i+2][j+2]) == couleur) and (plateau[i+3][j+3] is not None and getCouleurPion(plateau[i+3][j+3]) == couleur):
-    #
-    #             if i != 0 and j != 0:
-    #
-    #                 if plateau[i - 1][j - 1] is not None and getCouleurPion(plateau[i - 1][j - 1]) != couleur:
-    #                     res += [plateau[i][j], plateau[i + 1][j + 1], plateau[i + 2][j + 2], plateau[i + 3][j + 3]]
-    #                     # res += i, j, " ",i + 1, j + 1, " ",i + 2, j + 2, " ",i + 3, j + 3, " "
-    #
-    #             else:
-    #                 res += [plateau[i][j], plateau[i + 1][j + 1], plateau[i + 2][j + 2], plateau[i + 3][j + 3]]
-    #                 # res += i, j, " ", i + 1, j + 1, " ", i + 2, j + 2, " ", i + 3, j + 3, " "
-    #         j += 1
-    #     i += 1
-    #
-    # return res
-
     res = []
     i = const.NB_LINES-1
     while i >= 3:
@@ -305,4 +283,70 @@ def isRempliPlateau(plateau: list) -> bool:
     i = j = 0
     if plateau[i][j] is not None and plateau[i][j+1] is not None and plateau[i][j+2] is not None and plateau[i][j+3] is not None and plateau[i][j+4] is not None and plateau[i][j+5] is not None and plateau[i][j+6] is not None:
         res = True
+    return res
+
+# def placerPionLignePlateau(plateau: list, pion: dict, ligne: int, left: bool) -> tuple:
+#     """
+#     Fonction plaçant un pion sur la ligne indiquée soit par la gauche, soit par la droite
+#     :param plateau: Plateau dans lequel le pion va être inséré
+#     :param pion: Pion qui va être inséré
+#     :param ligne: Numéro de ligne dans laquelle le pion va être inséré
+#     :param left: Vaut True si le pion est inséré à gauche, False si inséré à droite
+#     :return: Retourne un tuple constitué de la liste des pions poussés et un entier correspond au numéro de ligne où se retrouve le dernier pion de la liste (None s'il ne change pas de ligne)
+#     :raise TypeError: Si le premier paramètre n'est pas un plateau
+#     :raise TypeError: Si le deuxième paramètre n'est pas un pion
+#     :raise TypeError: Si le troisième paramètre n'est pas un entier
+#     :raise ValueError: Si le troisième paramètre ne correspond pas à une ligne
+#     :raise TypeError: Si le quatrième paramètre n'est pas un booléen
+#     """
+#     if type_plateau(plateau) == False:
+#         raise TypeError("placerPionLignePlateau : Le premier paramètre n’est pas un plateau")
+#     if type_pion(pion) == False:
+#         raise TypeError("placerPionLignePlateau : Le second paramètre n’est pas un pion")
+#     if type(ligne) != int:
+#         raise TypeError("placerPionLignePlateau : le troisième paramètre n’est pas un entier")
+#     if ligne < 0 and ligne > (const.NB_LINES-1):
+#         raise ValueError(f"placerPionLignePlateau : Le troisième paramètre {ligne} ne désigne pas une ligne")
+#     if type(left) != bool:
+#         raise TypeError("placerPionLignePlateau : le quatrième paramètre n’est pas un booléen")
+#
+#     L = []
+#     if left == True:
+#         if plateau[ligne][0] is None:
+#             plateau[ligne][0] = pion
+#
+#
+#
+#
+#
+#     if left == False:
+#         if plateau[ligne][const.NB_COLUMNS-1] is None:
+#             plateau[ligne][const.NB_COLUMNS-1] = pion
+#
+#     return None
+
+
+
+def encoderPlateau(plateau: list) -> str:
+    """
+    Fonction permettant d'encoder un plateau
+    :param plateau: Plateau qui va être encodé
+    :return: Retourne une chaîne de caractères correspondant au plateau encodé
+    :raise TypeError: Si le paramètre n'est pas un plateau
+    """
+    if type_plateau(plateau) == False:
+        raise TypeError("encoderPlateau : Le paramètre n'est pas un plateau")
+
+    res = ''
+    for i in range(0, const.NB_LINES):
+
+        for j in range(0, const.NB_COLUMNS):
+
+            if plateau[i][j] is None:
+                res += "_"
+            elif plateau[i][j] is not None and getCouleurPion(plateau[i][j]) == const.ROUGE:
+                res += "R"
+            else:
+                res += "J"
+
     return res
